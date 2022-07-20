8cf8b09ec3932a7a1eb24650c337cfbc994c23ff
alyilmaz99 <aliyilmaz9977@gmail.com>
2022-07-21 00:32:27 +0300

    One click right and left add
    one click right and left option added

--- Assets/Codes/Kosu.cs
+++ Assets/Codes/Kosu.cs
@@ -8,18 +8,66 @@ public class Kosu : MonoBehaviour
     [SerializeField] float herspeed;
     
     float hor;
+    bool movementRight = true;
+    bool movementLeft = true;
+    int leftCount = 0;
+    int rightCount = 0;
 
-    void Start()
+    public void Start()
     {
         
     }
-
     
     void Update()
     {
-        hor = Input.GetAxis("Horizontal");
-        transform.Translate(new Vector3(hor * herspeed * Time.deltaTime, 0, movementspeed * Time.deltaTime));
+       
+        transform.Translate(new Vector3(0, 0, movementspeed * Time.deltaTime));
+        MovementOneClick();
+        
+        
+    }
 
+    void MovementOneClick()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (movementRight || leftCount == 0)
+            {
+                transform.Translate(new Vector3(3, 0, 0));
+                movementRight = false;
+                movementLeft = true;
+                rightCount++;
+                if (leftCount == 2)
+                {
+                    leftCount--;
+                }
+                else
+                {
+                    leftCount--;
+                }
 
+            }
+
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (movementLeft || rightCount == 0)
+            {
+                transform.Translate(new Vector3(-3, 0, 0));
+                movementLeft = false;
+                movementRight = true;
+                leftCount++;
+                if (rightCount == 2)
+                {
+                    rightCount--;
+                }
+                else
+                {
+                    rightCount--;
+                }
+            }
+
+        }
     }
 }
+
